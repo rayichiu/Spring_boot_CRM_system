7ebf72b20ecfbfdfac9e124c9a4b2f7a23d4ef3a
rayi <rayichiu@gmail.com>
2023-04-20 13:40:17 -0400

    adding code for 06-refactor-to-global-exception-handling

--- 01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -40,29 +40,5 @@ public class StudentRestController {
     // Getting element at index studentId
     return theStudents.get(studentId);
   }
-
-  // handle exception
-  @ExceptionHandler
-  public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-    // create a StudentErrorResponse
-    StudentErrorResponse error = new StudentErrorResponse();
-    error.setStatus(HttpStatus.NOT_FOUND.value());
-    error.setMessage(exc.getMessage());
-    error.setTimeStamp(System.currentTimeMillis());
-    // return ResponseEntity
-    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-  }
-
-  // add another exception handler ... to catch any exception (catch all)
-  @ExceptionHandler
-  public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
-    // create a StudentErrorResponse
-    StudentErrorResponse error = new StudentErrorResponse();
-    error.setStatus(HttpStatus.BAD_REQUEST.value());
-    error.setMessage(exc.getMessage());
-    error.setTimeStamp(System.currentTimeMillis());
-    // return ResponseEntity
-    return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-  }
   
 }
