23b041685b7f6250fceeed0eb883480c0ec76f18
rayi <rayichiu@gmail.com>
2023-04-21 22:42:55 -0400

    08-CRUD-initial commit

--- 02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -2,6 +2,8 @@ package com.luv2code.springboot.cruddemo.rest;
 
 import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,16 +13,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
-  private EmployeeDAO employeeDAO;
-  // quick and dirty: inject employee dao (use constructor injection)
-  // theEmployeeDAO generate by Spring Boot
-  public EmployeeRestController(EmployeeDAO theEmployeeDAO){
-    employeeDAO = theEmployeeDAO;
+
+//  private EmployeeDAO employeeDAO;
+//  // quick and dirty: inject employee dao (use constructor injection)
+//  // theEmployeeDAO generate by Spring Boot
+//  public EmployeeRestController(EmployeeDAO theEmployeeDAO){
+//    employeeDAO = theEmployeeDAO;
+//  }
+
+
+  // instead using service to follow the application architecture
+  private EmployeeService employeeService;
+  @Autowired
+  public EmployeeRestController(EmployeeService theEmployeeService){
+    employeeService = theEmployeeService;
   }
 
   // expose "/employees" and return a list of employees
   @GetMapping("/employees")
   public List<Employee> findAll(){
-    return employeeDAO.findAll();
+    return employeeService.findAll();
   }
 }
---
+++ 02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.luv2code.springboot.cruddemo.service;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+  List<Employee> findAll();
+}
