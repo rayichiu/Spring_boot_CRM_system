19d754be201f0b0fa93aff075e7866bbf959a587
rayi <rayichiu@gmail.com>
2023-04-25 14:46:50 -0400

    refactor the hard coded user info to DB query

--- 03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -7,10 +7,24 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.management.MXBean;
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
+  // add support for JDBC ... no more hardcoded users
+
+  @Bean
+  UserDetailsManager userDetailsManager(DataSource dataSource){
+    return new JdbcUserDetailsManager(dataSource);
+  }
+
+
+/* comment out the hard coded users
   @Bean
   public InMemoryUserDetailsManager userDetailsManager() {
     UserDetails john = User.builder() .username("john")
@@ -27,6 +41,8 @@ public class DemoSecurityConfig {
         .build();
     return new InMemoryUserDetailsManager(john, mary, susan);
   }
+
+ */
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
