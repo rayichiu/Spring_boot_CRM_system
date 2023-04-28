608278af15c3ff7695809716bf4cc0e246901c35
rayi <rayichiu@gmail.com>
2023-04-28 13:46:26 -0400

    add frontendframework and get method

--- 03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ 03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -22,7 +22,7 @@ public class EmployeeServiceImpl implements EmployeeService{
 
   @Override
   public List<Employee> findAll() {
-    return employeeRepository.findAll();
+    return employeeRepository.findAllByOrderByLastNameAsc();
   }
 
   // delegate the call to DAO
